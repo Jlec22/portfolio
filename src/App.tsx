3ddb25c386b5903893dad72b61d02b01556aee18
leclouxj <wblgilbert@gmail.com>
2026-02-16 13:28:04 -0600

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ maxWidth: "800px", margin: "auto", fontFamily: "Arial" }}>
+      <h1>Jack LeCloux</h1>
+      <p>Computer Science Student</p>
+
+      <h2>Projects</h2>
+
+      <div>
+        <h3>TITLE PLACEHOLDER</h3>
+        <p>body</p>
+      </div>
+
+      <div>
+        <h3>TITLE</h3>
+        <p>body</p>
+      </div>
+
+      <h2>Contact</h2>
+      <p>Email: your@email.com</p>
+    </div>
+  );
+}
+
+export default App;
+
